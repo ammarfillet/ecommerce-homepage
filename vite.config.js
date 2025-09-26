cbcba17e7268ca0c41fdd0f16893271b329d21a4
ammarfillet <ammarsydn22@gmail.com>
2025-09-26 14:47:40 +0700

    Configure Vite base for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/ecommerce-homepage/',
   plugins: [react()],
   // Untuk development, gunakan base: '/'
   // Untuk GitHub Pages, gunakan base: '/ecommerce-homepage/'
